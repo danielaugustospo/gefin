66837129db3b83a1d96594b694a5264ca608692c
unknown <danielaugustospo@gmail.com>
2021-11-24 10:55:08 -0300

    Primeiro commit

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=31.220.60.44;dbname=prd_gefinpessoal',
+    'username' => 'root',
+    'password' => '8523654*dA',
+    'charset' => 'utf8',
+    'enableSchemaCache' => true,
+];
